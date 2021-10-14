f9f606435822ed6c8b9a99b5855af61ac399eab0
jun951231 <jun951231@gmail.com>
2021-10-14 17:52:39 +0900

    Oct 14

---
+++ backend/admin/user/__init__.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from icecream import ic
+import time
+from datetime import datetime
+print('=========')
+print(f'======Django REST Framework {datetime.now()}')
+print('=========')
